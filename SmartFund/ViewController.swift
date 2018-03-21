b35d6bcd39853425a79a87ab5e4dfdb1cb59d8c0
jansenfan <jansen_fan@xiaomantou.local>
2018-03-21 11:37:48 +0800

    Initial Commit

---
+++ SmartFund/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SmartFund
+//
+//  Created by jansen_fan on 2018/3/21.
+//  Copyright © 2018年 jansen_fan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
